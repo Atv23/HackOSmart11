0c9257196fac3cee5bab93e1971411d063e65524
Vaibhav-Singh2110 <vaibhavmsd39@gmail.com>
2023-05-02 02:43:17 +0530

    schooldetails

---
+++ backend/routes/schoolinfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const schoolinfo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default schoolinfo
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import School from './components/School'
 import College from './components/College'
+import SchoolDetails from './components/SchoolDetails';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <School></School>
+       <SchoolDetails></SchoolDetails>
        {/* <College></College> */}
       </header>
     </div>
